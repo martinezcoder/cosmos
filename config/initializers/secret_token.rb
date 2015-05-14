ceffd837c462e407b2034993c34d5123be85f85c
Fran J Martinez <martinezcoder@gmail.com>
2015-05-14 12:47:47 +0200

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cosmos::Application.config.secret_key_base = 'd7cf646b4c51a16d8877047d55d73634b412da342a282eafa74baae752fb4dd194940d78e16c6389c0851b302058440d607759b0bd129b394aa00af09d036b51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cosmos::Application.config.session_store :cookie_store, key: '_cosmos_session'
